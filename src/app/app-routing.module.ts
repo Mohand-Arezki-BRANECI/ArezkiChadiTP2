e500fa8373398b0ebb6664a5de7063f4a978021e
Chatdi <chadi.grolleauraoux@gmail.com>
2023-09-25 16:34:24 +0200

    navigation faite

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {ListeComponent} from "./liste/liste.component";
+import { GestionComponent } from './gestion/gestion.component';
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+  { path: '', redirectTo:"/home", pathMatch:"full"},
+  { path: 'home', component: HomeComponent },
+  { path: 'liste', component: ListeComponent },
+  { path: 'gestion', component: GestionComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,12 +9,7 @@ import { HomeComponent } from './home/home.component';
 import { ListeComponent } from './liste/liste.component';
 import { GestionComponent } from './gestion/gestion.component';
 
-const routes: Routes = [
-  { path: '', redirectTo:"/home", pathMatch:"full"},
-  { path: 'home', component: HomeComponent },
-  { path: 'liste', component: ListeComponent },
-  { path: 'gestion', component: GestionComponent },
-];
+
 @NgModule({
   declarations: [
     AppComponent,
